208a40ca65a0eb92ad3325523774bea92aed0989
davidwagura <davidwagura83@gmail.com>
2023-03-30 13:27:21 +0300

    add a button that displays full name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-new vue ({
+new Vue ({
   el:'#function',
+
   data: {
-    message: 'Hello vue!'
+    message: 'Hello vue!',
+    first_name:'David',
+    second_name:'wagura',
+    fullName: ''
   },
+
   computed: {},
 
   methods: {
     changeMessage() {
       this.message = 'My functions'
+    },
+    getFullName() {
+      this.fullName = this.first_name + " " + this.second_name;
+    },
+
+    newMethod(){
+      console.log('testing')
     }
+
   }
+
 })
